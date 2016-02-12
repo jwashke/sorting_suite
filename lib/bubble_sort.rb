5a156cc5ab515f53b568ee4a136a48dca413a8dd
Josh Washke <jwashke@icloud.com>
2016-02-11 21:24:14 -0700

    Added bubble_sort testing suite and bubble_sort implementation.

---
+++ lib/bubble_sort.rb
@@ -0,0 +1,29 @@
+require 'pry'
+require 'pry-nav'
+
+class BubbleSort
+  def initialize
+    @swapping = true
+  end
+
+  def sort(array)
+    @swapping = true
+    while @swapping
+      @swapping = false
+      array = make_a_pass_through_the_array(array)
+    end
+    array
+  end
+
+  #private
+
+  def make_a_pass_through_the_array(array)
+    (array.length - 1).times do |index|
+      if array[index] > array[index + 1]
+        array[index], array[index + 1] = array[index + 1], array[index]
+        @swapping = true
+      end
+    end
+    array
+  end
+end
