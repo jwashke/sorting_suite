0d550c159a369c615c475beb8e0a9efd240fa229
Josh Washke <jwashke@icloud.com>
2016-02-15 13:34:50 -0700

    Implemented selection sort and accompanying tests.

---
+++ lib/selection_sort.rb
@@ -0,0 +1,20 @@
+module SortingSuite
+  class SelectionSort
+
+    def sort(array)
+      0.upto(array.length - 2) do |index|
+        sort_inner_loop(array, index)
+      end
+      array
+    end
+
+    def sort_inner_loop(array, index)
+      min = index
+      (index + 1).upto(array.length - 1) do |jndex|
+        min = jndex  if array[jndex] < array[min]
+      end
+      array[index], array[min] = array[min], array[index] if index != min
+      array
+    end
+  end
+end
---
+++ test/selection_sort_test.rb
@@ -0,0 +1,44 @@
+gem 'minitest'
+require 'minitest/autorun'
+require_relative '../lib/selection_sort'
+
+class SelectionSortTest < Minitest::Test
+
+  def setup
+    @sorter = SortingSuite::SelectionSort.new
+  end
+
+  def test_it_can_create_an_object_of_itself
+    assert_equal SortingSuite::SelectionSort, @sorter.class
+  end
+
+  def test_it_can_sort_an_array
+    assert_equal [1, 2, 3, 4], @sorter.sort([4, 2, 1, 3])
+  end
+
+  def test_it_can_sort_a_reversed_array
+    assert_equal [1, 2, 3, 4], @sorter.sort([4, 3, 2, 1])
+  end
+
+  def test_it_can_sort_a_presorted_array
+    assert_equal [1, 2, 3, 4], @sorter.sort([1, 2, 3, 4])
+  end
+
+  def test_it_can_sort_an_empty_array
+    assert_equal [], @sorter.sort([])
+  end
+
+  def test_it_can_sort_a_one_element_array
+    assert_equal [1], @sorter.sort([1])
+  end
+
+  def test_it_can_sort_a_very_large_array
+    sorted_large_array = (1..1000).to_a
+    shuffled_large_array = sorted_large_array.shuffle
+    assert_equal sorted_large_array, @sorter.sort(shuffled_large_array)
+  end
+
+  def test_it_can_sort_an_array_of_letters
+    assert_equal ["a", "b", "c", "d"], @sorter.sort(["d", "b", "a", "c"])
+  end
+end
