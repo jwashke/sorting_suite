4303ac48ef0de3e4191341e0712e31e7c9418106
Josh Washke <jwashke@icloud.com>
2016-02-15 18:08:10 -0700

    made insertion prettier

--- lib/insertion_sort.rb
+++ lib/insertion_sort.rb
@@ -5,23 +5,17 @@ module SortingSuite
     end
 
     def sort(array)
-      until array.empty?
-        temp = array.shift
-        if @sorted.empty?
-          @sorted.push(temp)
-        else
-          was_sorted = false
-          @sorted.each_with_index do |element, index|
-            if temp < element
-              @sorted.insert(index, temp)
-              was_sorted = true
-              break
-            end
-          end
-          @sorted.push(temp) if was_sorted == false
-        end
+      array.each do |unsorted_element|
+        @sorted.insert(find_index(unsorted_element), unsorted_element)
       end
       @sorted
+    end
+
+    def find_index(unsorted_element)
+      @sorted.each_with_index do |sorted_element, index|
+        return index if unsorted_element < sorted_element
+      end
+      -1
     end
   end
 end
