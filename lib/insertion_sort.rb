62edbe73329e47065dcf4a131671d5076dad4faa
Josh Washke <jwashke@icloud.com>
2016-02-15 17:26:44 -0700

    Everythings working

--- lib/insertion_sort.rb
+++ lib/insertion_sort.rb
@@ -1,44 +1,27 @@
 module SortingSuite
   class InsertionSort
+    def initialize
+      @sorted = []
+    end
 
     def sort(array)
-      sorted = []
       until array.empty?
         temp = array.shift
-        if sorted.empty?
-          sorted.push(temp)
+        if @sorted.empty?
+          @sorted.push(temp)
         else
           was_sorted = false
-          sorted.each_with_index do |element, index|
+          @sorted.each_with_index do |element, index|
             if temp < element
-              sorted.insert(index, temp)
+              @sorted.insert(index, temp)
               was_sorted = true
               break
             end
           end
-          sorted.push(temp) if was_sorted == false
+          @sorted.push(temp) if was_sorted == false
         end
       end
-      sorted
-    end
-
-    def inplace_sort(array)
-      array.length.times do |index|
-        jndex = index
-        array = sort_inner_loop(array, index)
-      end
-      array
-    end
-
-    private
-
-    def sort_inner_loop(array, index)
-      jndex = index
-      while jndex > 0 and array[jndex - 1] > array[jndex]
-        array[jndex - 1], array[jndex] = array[jndex], array[jndex - 1]
-        jndex -= 1
-      end
-      array
+      @sorted
     end
   end
 end
